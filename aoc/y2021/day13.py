e62777fdf85042cef61c88c48708b3032559140f
Ryan Crawford <ryan.crawford@exptechinc.com>
2021-12-14 08:36:51 -0500

    Day 13/14 at Home

---
+++ aoc/y2021/day13.py
@@ -0,0 +1,93 @@
+""" Day 13 Solutions """
+
+import sys
+import numpy as np
+from aoc.y2021.utils import load_data
+
+
+def pageprint(page):
+    print()
+    for row in page:
+        x = ""
+        for char in row:
+            x += "#" if char else "."
+        print(x)
+
+
+def solve(d):
+    """actual solution with puzzle input"""
+    result_1, result_2 = 0, 0
+    print("INPUT DATA:")
+    print(d)
+    xs, ys = [], []
+    folds = []
+    for row in d:
+        if "," in row:
+            x, y = list(map(int, row.split(",")))
+            xs.append(x)
+            ys.append(y)
+        if "fold" in row:
+            dir, dist = row.split(" ")[-1].split("=")
+            folds.append((dir, int(dist)))
+
+    print(list(zip(xs, ys)))
+    print(folds)
+    paper = np.zeros((max(ys) + 1, max(xs) + 1), dtype=np.bool)
+    for x, y in zip(xs, ys):
+        paper[y, x] = True
+    pageprint(paper)
+    ct = 0
+    for direction, dist in folds:
+        ct += 1
+        print(direction, dist)
+        if direction == "x":
+            a = paper[:, :dist]
+            b = paper[:, -dist:]
+            # pageprint(a)
+            # pageprint(b)
+            paper = a | b[:, ::-1]
+            # pageprint(paper)
+        if direction == "y":
+            a = paper[:dist, :]
+            b = paper[-dist:, :]
+            # print("TOP")
+            # pageprint(a)
+            # print("BOTTOM")
+            # pageprint(b)
+            paper = a | b[::-1, :]
+            # print("FOLDED")
+            # pageprint(paper)
+        if ct == 1:
+            result_1 = np.sum(paper)
+
+    pageprint(paper)
+
+    return result_1, result_2
+
+
+def main():
+    """Main function"""
+    # load data:
+    skip_test = False
+    if not skip_test:
+        print("**** TEST DATA ****")
+        d = load_data("test_day13.txt")
+        test_answer_1 = 17
+        test_answer_2 = 0
+        test_solution_1, test_solution_2 = solve(d)
+        assert test_solution_1 == test_answer_1, f"TEST #1 FAILED: TRUTH={test_answer_1}, YOURS={test_solution_1}"
+        assert test_solution_2 == test_answer_2, f"TEST #2 FAILED: TRUTH={test_answer_2}, YOURS={test_solution_2}"
+        print("**** TESTS PASSED ****")
+        print("Test Answer 1: ", test_answer_1)
+        print("My Test Answer 1: ", test_solution_1)
+        print("Test Answer 2: ", test_answer_2)
+        print("My Test Answer 2: ", test_solution_2)
+    print("**** REAL DATA ****")
+    d = load_data("day13.txt")
+    answer_1, answer_2 = solve(d)
+    print("Answer 1:", answer_1)
+    print("Answer 2:", answer_2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ aoc/y2021/day14.py
@@ -0,0 +1,96 @@
+""" Day 14 Solutions """
+
+import sys
+from typing import Counter
+import numpy as np
+from aoc.y2021.utils import load_data
+
+
+def digraphs_in(s):
+    return [s[idx : idx + 2] for idx in range(len(s) - 1)]
+
+
+def solve(d):
+    """actual solution with puzzle input"""
+    result_1, result_2 = 0, 0
+    print("INPUT DATA:")
+    print(d)
+    string: str = d[0]
+    initial_str = d[0]
+    transforms = []
+    for row in d[2:]:
+        transforms.append(row.split(" -> "))
+    print(transforms)
+    print(string)
+    rounds = []
+    rounds_ct = []
+    for _ in range(10):
+        holder = [
+            "",
+        ] * len(string)
+        for pair, letter in transforms:
+            try:
+                for idx in range(len(string) - 1):
+                    if string[idx : idx + 2] == pair:
+                        holder[idx] = letter
+            except ValueError:
+                pass
+        string = "".join([k + l for (k, l) in zip(string, holder)])
+        rounds.append(Counter(digraphs_in(string)))
+        rounds_ct.append({x: string.count(x) for x in set(string)})
+    counts = sorted([string.count(x) for x in set(string)])
+    result_1 = counts[-1] - counts[0]
+
+    di_list = digraphs_in(initial_str)
+    counts = Counter(di_list)
+
+    transform_map = {pair: letter for (pair, letter) in transforms}
+    for step in range(0, 40):
+        new_counts = Counter()
+        for digraph in counts:
+            insertion = transform_map[digraph]
+            new_tri = digraph[0] + insertion + digraph[1]
+            new_counts[new_tri[:2]] += counts[digraph]
+            new_counts[new_tri[1:]] += counts[digraph]
+        counts = new_counts
+
+        letters = set(out for _, out in transforms)
+        count_per_letter = {l: 0 for l in letters}
+        for digraph in counts:
+            for letter in count_per_letter:
+                count_per_letter[letter] += counts[digraph] * digraph.count(letter)
+        totals = sorted(v for _, v in count_per_letter.items())
+        if step == 9:
+            result_1 = (totals[-1] - totals[0]) // 2 + 1
+        if step == 39:
+            result_2 = (totals[-1] - totals[0]) // 2 + 1
+
+    return result_1, result_2
+
+
+def main():
+    """Main function"""
+    # load data:
+    skip_test = False
+    if not skip_test:
+        print("**** TEST DATA ****")
+        d = load_data("test_day14.txt")
+        test_answer_1 = 1588
+        test_answer_2 = 2188189693529
+        test_solution_1, test_solution_2 = solve(d)
+        assert test_solution_1 == test_answer_1, f"TEST #1 FAILED: TRUTH={test_answer_1}, YOURS={test_solution_1}"
+        assert test_solution_2 == test_answer_2, f"TEST #2 FAILED: TRUTH={test_answer_2}, YOURS={test_solution_2}"
+        print("**** TESTS PASSED ****")
+        print("Test Answer 1: ", test_answer_1)
+        print("My Test Answer 1: ", test_solution_1)
+        print("Test Answer 2: ", test_answer_2)
+        print("My Test Answer 2: ", test_solution_2)
+    print("**** REAL DATA ****")
+    d = load_data("day14.txt")
+    answer_1, answer_2 = solve(d)
+    print("Answer 1:", answer_1)
+    print("Answer 2:", answer_2)
+
+
+if __name__ == "__main__":
+    main()
