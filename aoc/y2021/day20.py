e09f0bd8a55f05753a69f0a02375aab9aff26c3e
Ryan Crawford <ryan.crawford@exptechinc.com>
2021-12-20 19:32:00 -0500

    Clean Day 20

--- aoc/y2021/day20.py
+++ aoc/y2021/day20.py
@@ -16,8 +16,6 @@ NBR8 = [
 
 def solve(d):
     """actual solution with puzzle input"""
-    result_1, result_2 = 0, 0
-
     key = d[0]
     img_on = set()
     for idx, row in enumerate(d[2:]):
@@ -28,21 +26,21 @@ def solve(d):
     # assuming square grid everywhere:
     grid_sz = len(d) - 1
     for step in range(50):
-        low, high = -1 - step, grid_sz + step
-        # if we have the '#' at zero, add two rings of "#'s":
+        # we'll alternate tracking #'s or .'s if key[0] = '#'
+        char, t_char, f_char = "#", "1", "0"
         if key[0] == "#":
-            for x in (low, low - 1, high, high + 1):
-                for y in range(low - 1, high + 3):
-                    if step % 2 == 1:
-                        img_on.add((x, y))
-                        img_on.add((y, x))
-        new_img = set()
+            if step % 2 == 0:
+                char = "."
+            else:
+                t_char, f_char = ("0", "1")
         # compute one step outward in x/y each step:
-        # the infinite cells will not matter outside of this ring on the even steps:
+        # the infinite cells will not matter outside of this ring on the alternating steps:
+        low, high = -1 - step, grid_sz + step
+        new_img = set()
         for x in range(low, high + 1):
             for y in range(low, high + 1):
-                word = "".join(["1" if (x + n[0], y + n[1]) in img_on else "0" for n in NBR8])
-                if key[int(word, 2)] == "#":
+                word = "".join([t_char if (x + n[0], y + n[1]) in img_on else f_char for n in NBR8])
+                if key[int(word, 2)] == char:
                     new_img.add((x, y))
         img_on = new_img
 
@@ -56,12 +54,12 @@ def solve(d):
 def main():
     """Main function"""
     # load data:
-    skip_test = True
+    skip_test = False
     if not skip_test:
         print("**** TEST DATA ****")
         d = load_data("test_day20.txt")
         test_answer_1 = 35
-        test_answer_2 = 0
+        test_answer_2 = 3351
         test_solution_1, test_solution_2 = solve(d)
         assert test_solution_1 == test_answer_1, f"TEST #1 FAILED: TRUTH={test_answer_1}, YOURS={test_solution_1}"
         assert test_solution_2 == test_answer_2, f"TEST #2 FAILED: TRUTH={test_answer_2}, YOURS={test_solution_2}"
@@ -73,8 +71,8 @@ def main():
     print("**** REAL DATA ****")
     d = load_data("day20.txt")
     answer_1, answer_2 = solve(d)
-    assert answer_1 == 5291
-    assert answer_2 == 16665
+    assert answer_1 == 5291, f"{answer_1} != 5291"
+    assert answer_2 == 16665, f"{answer_1} != 16665"
     print("Answer 1:", answer_1)
     print("Answer 2:", answer_2)
 
