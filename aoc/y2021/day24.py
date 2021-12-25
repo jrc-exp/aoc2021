b60b3484ffdc22750c50e1958cd34bd69b9cd307
Ryan Crawford <ryan.crawford@exptechinc.com>
2021-12-25 12:24:03 -0500

    Optimizing Day 24

--- aoc/y2021/day24.py
+++ aoc/y2021/day24.py
@@ -17,67 +17,78 @@ class ALU:
     """ALU"""
 
     def __init__(self) -> None:
-        self.w = 0
-        self.x = 0
-        self.y = 0
-        self.z = 0
+        self.reset()
+        self.func_map = {
+            "inp": self.inp,
+            "add": self.add,
+            "mul": self.mul,
+            "div": self.div,
+            "mod": self.mod,
+            "eql": self.eql,
+        }
 
-    def set(self, var, val):
-        setattr(self, var, val)
+    @property
+    def x(self):
+        return self.vars["x"]
+
+    @property
+    def y(self):
+        return self.vars["y"]
+
+    @property
+    def w(self):
+        return self.vars["w"]
+
+    @property
+    def z(self):
+        return self.vars["z"]
 
     def run(self, op):
-        func, a, b = op.split(" ")
-        func = self.__getattribute__(func)
+        func, a, b = op
+        func = self.func_map[func]
         func(a, b)
 
-    def get(self, var):
-        return self.__getattribute__(var)
-
     def parse(self, var1, var2):
-        try:
-            val2 = int(var2)
-        except ValueError:
-            val2 = self.get(var2)
-        val1 = self.get(var1)
+        val2 = self.vars.get(var2, var2)
+        val1 = self.vars[var1]
         return val1, val2
 
     def inp(self, var, val):
-        self.set(var, int(val))
+        self.vars[var] = val
 
     def add(self, var1, var2):
         a, b = self.parse(var1, var2)
         val = a + b
-        self.set(var1, val)
+        self.vars[var1] = val
 
     def mul(self, var1, var2):
         a, b = self.parse(var1, var2)
         val = a * b
-        self.set(var1, val)
+        self.vars[var1] = val
 
     def div(self, var1, var2):
         a, b = self.parse(var1, var2)
         assert b > 0
         val = trunc(a / b)
-        self.set(var1, val)
+        self.vars[var1] = val
 
     def mod(self, var1, var2):
         a, b = self.parse(var1, var2)
         assert b > 0
         assert a >= 0
         val = a % b
-        self.set(var1, val)
+        self.vars[var1] = val
 
     def eql(self, var1, var2):
         a, b = self.parse(var1, var2)
         val = 1 if a == b else 0
-        self.set(var1, val)
+        self.vars[var1] = val
 
     def reset(self):
-        for v in ["x", "y", "z", "w"]:
-            self.set(v, 0)
+        self.vars = {v: 0 for v in "xyzw"}
 
     def __repr__(self):
-        return f"ALU: x={self.x}, y={self.y}, z={self.z}, w={self.w}"
+        return f"ALU: x={self.vars['x']}, y={self.vars['y']}, z={self.vars['z']}, w={self.vars['w']}"
 
 
 def run_program(program, input_num, stop_input=0):
@@ -87,10 +98,10 @@ def run_program(program, input_num, stop_input=0):
     input_num = str(input_num)
     input_ct = 0
     for op in program:
-        if op.startswith("inp"):
+        if op[0] == "inp":
             if stop_input and stop_input == input_ct:
                 break
-            op += " " + input_num[input_off]
+            op[-1] = int(input_num[input_off])
             input_off += 1
             input_ct += 1
         alu.run(op)
@@ -98,64 +109,78 @@ def run_program(program, input_num, stop_input=0):
     return alu
 
 
-def solve(d):
+def clean_program(d):
+    """pre-compute some stuff for speed later"""
+    for idx, row in enumerate(d):
+        d[idx] = row.split(" ")
+        for idy, c in enumerate(d[idx]):
+            try:
+                d[idx][idy] = int(c)
+            except:
+                pass
+            if len(d[idx]) == 2:
+                d[idx].append("")
+    return d
+
+
+def solve(d, skip_tests=False):
     """actual solution with puzzle input"""
     result_1, result_2 = 0, 0
-    print("INPUT DATA:")
-    print(d)
+    d = clean_program(d)
 
-    input_num = 13579246899999
+    if not skip_tests:
+        input_num = 13579246899999
+        program = ["inp x", "mul x -1"]
+        program = clean_program(program)
+        alu = run_program(program, input_num)
+        assert alu.z == 0
+        program = ["inp z", "inp x", "mul z 3", "eql z x"]
+        program = clean_program(program)
+        alu = run_program(program, input_num)
+        assert alu.z == 1
 
-    program = ["inp x", "mul x -1"]
-    z = run_program(program, input_num)
-    print(z, program)
-
-    program = ["inp z", "inp x", "mul z 3", "eql z x"]
-    z = run_program(program, input_num)
-    print(z, program)
-
-    program = [
-        "inp w",
-        "add z w",
-        "mod z 2",
-        "div w 2",
-        "add y w",
-        "mod y 2",
-        "div w 2",
-        "add x w",
-        "mod x 2",
-        "div w 2",
-        "mod w 2",
-    ]
-    for num in range(1, 10):
-        alu = run_program(program, num, False)
-        bin_num = format(num, "04b")
-        assert alu.z == int(bin_num[3]), "z"
-        assert alu.w == int(bin_num[0]), "w"
-        assert alu.x == int(bin_num[1]), "x"
-        assert alu.y == int(bin_num[2]), "y"
+        program = [
+            "inp w",
+            "add z w",
+            "mod z 2",
+            "div w 2",
+            "add y w",
+            "mod y 2",
+            "div w 2",
+            "add x w",
+            "mod x 2",
+            "div w 2",
+            "mod w 2",
+        ]
+        program = clean_program(program)
+        for num in range(1, 10):
+            alu = run_program(program, num, False)
+            bin_num = format(num, "04b")
+            assert alu.z == int(bin_num[3]), "z"
+            assert alu.w == int(bin_num[0]), "w"
+            assert alu.x == int(bin_num[1]), "x"
+            assert alu.y == int(bin_num[2]), "y"
     # pylint: disable=pointless-string-statement
 
-    # I'm not sure why this worked, but it did:
+    # I'm not entirely sure why this worked, but it did:
     best_z = np.inf
+    # it doesn't actually matter what number you start with:
+    # this seems to find the minimum number directly
     best_num = int("".join(["9"] * 14))
     for stop_input in range(1, 15):
-        # print("stop after", stop_input)
         best_z = run_program(d, best_num).z
-        for _ in range(10):
-            for digit in reversed(range(14)):
-                for n in reversed(range(1, 10)):
-                    num = list(str(best_num))
-                    num[digit] = str(n)
-                    num = int("".join(num))
-                    alu = run_program(d, num, stop_input=stop_input)
-                    # print(num, alu.z)
-                    if alu.z < best_z:
-                        best_num = num
-                        best_z = alu.z
-                        print(best_num, best_z)
-                        if best_z == 0:
-                            print("found one!")
+        for digit in reversed(range(14)):
+            for n in reversed(range(1, 10)):
+                num = list(str(best_num))
+                num[digit] = str(n)
+                num = int("".join(num))
+                alu = run_program(d, num, stop_input=stop_input)
+                if alu.z < best_z:
+                    best_num = num
+                    best_z = alu.z
+                    if best_z == 0 and stop_input == 14:
+                        print("Found a zero!")
+                        print(best_num, best_z, alu)
 
     # But once we have a "zero" number we can find the bigger and
     # smaller ones easily because the output is only ever affected by
@@ -175,11 +200,11 @@ def solve(d):
                 if alu.z == 0:
                     changed = True
                     best_num = num
-                    print(best_num, "bigger and better")
                     break
-    print("Result 1", best_num)
-
     result_1 = best_num
+
+    # now we search for the min, but it was apparently what we found
+    # earlier!
     changed = True
     while changed:
         changed = False
@@ -194,10 +219,8 @@ def solve(d):
                 alu = run_program(d, num, stop_input=stop_input)
                 if alu.z == 0:
                     best_num = num
-                    print(best_num, "smaller and faster")
                     changed = True
     result_2 = best_num
-    print("Result 2", best_num)
 
     return result_1, result_2
 
